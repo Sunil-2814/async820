d91a5900fb3ce9eca902d1eebf4246ed1ef27971
Sunil-2814 <sunilmali274@gmail.com>
2023-04-19 16:09:48 +0530

    write down some functions to understand callback

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,18 @@
 let cl = console.log;
-const box = document.getElementById("box")
-const btn = document.getElementById("btn")
+// const box = document.getElementById("box")
+// const btn = document.getElementById("btn")
 
-let flag = true;
+// let flag = true;
 
-btn.addEventListener("click", function(){
-    if(flag === true){
-        box.style.display = "none";
-        flag = false;
-    }else{
-        box.style.display = "block";
-        flag = true;
-    }
-})
+// btn.addEventListener("click", function(){
+//     if(flag === true){
+//         box.style.display = "none";
+//         flag = false;
+//     }else{
+//         box.style.display = "block";
+//         flag = true;
+//     }
+// })
+function print10(){
+    cl(20)
+}
